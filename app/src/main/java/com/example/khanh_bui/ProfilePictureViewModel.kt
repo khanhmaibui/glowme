467176f7fa7039f8836b6d86793fd9de094b0591
kmbui <mkb18@sfu.ca>
2023-12-27 23:04:23 -0800

    add map activity and activity classifier

--- app/src/main/java/com/example/khanh_bui/ProfilePictureViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.example.khanh_bui
-
-import android.graphics.Bitmap
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-
-class ProfilePictureViewModel: ViewModel() {
-    val userImage = MutableLiveData<Bitmap>()
-}
